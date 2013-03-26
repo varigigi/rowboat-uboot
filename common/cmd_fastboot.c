varigigi/rowboat-uboot

commit 9da67a1093b1292333d1760485b7a595ba336392

Rename CONFIG_USB_AM335X to CONFIG_USB_MUSB_AM335X
Make way for musb-new implementation

Signed-off-by: Vishveshwar Bhat <vishveshwar.bhat@ti.com>

include/configs/am335x_evm.h
@@ -78,13 +78,13 @@
  * Enable CONFIG_MUSB_HCD for Host functionalities MSC, keyboard
  * Enable CONFIG_MUSB_UDC for Device functionalities.
  */
-#define CONFIG_USB_AM335X                1
+#define CONFIG_USB_MUSB_AM335X           1
 
 /* #define CONFIG_MUSB_HCD                      1 */
 
 #define CONFIG_MUSB_UDC         1
 
-#ifdef CONFIG_USB_AM335X
+#ifdef CONFIG_USB_MUSB_AM335X
 
 #ifdef CONFIG_MUSB_HCD
 #define CONFIG_CMD_USB

drivers/usb/musb/Makefile
@@ -32,7 +32,7 @@ COBJS-$(CONFIG_USB_DAVINCI) += davinci.o
 COBJS-$(CONFIG_USB_OMAP3) += omap3.o
 COBJS-$(CONFIG_USB_DA8XX) += da8xx.o
 COBJS-$(CONFIG_USB_AM35X) += am35x.o
-COBJS-$(CONFIG_USB_AM335X) += am335x.o
+COBJS-$(CONFIG_USB_MUSB_AM335X) += am335x.o
 
 COBJS	:= $(COBJS-y)
 SRCS	:= $(COBJS:.o=.c)

include/usb.h
@@ -140,7 +140,7 @@ struct usb_device {
 	defined(CONFIG_USB_R8A66597_HCD) || defined(CONFIG_USB_DAVINCI) || \
 	defined(CONFIG_USB_OMAP3) || defined(CONFIG_USB_DA8XX) || \
 	defined(CONFIG_USB_BLACKFIN) || defined(CONFIG_USB_AM35X) || \
-	defined(CONFIG_USB_AM335X)
+	defined(CONFIG_USB_MUSB_AM335X)
 
 int usb_lowlevel_init(void);
 int usb_lowlevel_stop(void);

commit e5521fb3eebfb062073d0e3dd02f70b4c8994aec

FASTBOOT: Fix on AM335x
Fastboot was broken after commit 9da67a1
Based on fix suggested at
http://e2e.ti.com/support/arm/sitara_arm/f/791/p/161948/890182.aspx#889723

Also fix serial number showing as ????????????

Change-Id: Ide99eb202fca82d1f6a3afdceb812ddc6ce1fe82
Signed-off-by: Vishveshwar Bhat <vishveshwar.bhat@ti.com>

common/cmd_fastboot.c
@@ -171,6 +171,7 @@ static struct usb_device_descriptor device_descriptor = {
 	.bDeviceProtocol =	0x00,
 	.bMaxPacketSize0 =	EP0_MAX_PACKET_SIZE,
 	.idVendor =		cpu_to_le16(CONFIG_USBD_VENDORID),
+	.idProduct =		cpu_to_le16(CONFIG_USBD_PRODUCTID),
 	.bcdDevice =		cpu_to_le16(USBFBT_BCD_DEVICE),
 	.iManufacturer =	STR_MANUFACTURER,
 	.iProduct =		STR_PRODUCT,
@@ -1337,10 +1338,12 @@ static void set_serial_number(void)
 {
 	char *dieid = getenv("dieid#");
 	if (dieid == NULL) {
-		priv.serial_no = "00123";
+		printf("Setting serial number from constant (no dieid info)\n");
+		strncpy(serial_number, "00123", sizeof("00123"));
 	} else {
 		int len;
 
+		printf("Setting serial number from unique id\n");
 		memset(&serial_number[0], 0, 28);
 		len = strlen(dieid);
 		if (len > 28)
@@ -1348,8 +1351,9 @@ static void set_serial_number(void)
 
 		strncpy(&serial_number[0], dieid, len);
 
-		priv.serial_no = &serial_number[0];
 	}
+	priv.serial_no = serial_number;
+	printf("fastboot serial_number = %s\n", serial_number);
 }
 
 static int fbt_fastboot_init(void)

drivers/usb/musb/musb_udc.c
@@ -59,7 +59,7 @@
 #include "omap3.h"
 #elif defined(CONFIG_USB_AM35X)
 #include "am35x.h"
-#elif defined(CONFIG_USB_AM335X)
+#elif defined(CONFIG_USB_MUSB_AM335X)
 #include "am335x.h"
 #elif defined(CONFIG_USB_DAVINCI)
 #include "davinci.h"
@@ -100,7 +100,7 @@ do {									\
 extern struct am35x_usb_regs *regs;
 #endif
 
-#ifdef	CONFIG_USB_AM335X
+#ifdef	CONFIG_USB_MUSB_AM335X
 extern struct am335x_usb_regs *regs;
 #endif
 
@@ -813,7 +813,7 @@ void udc_irq(void)
 			regs->ep_intsrcclr = intr_ep;
 			intrrx = (intr_ep & 0xFFFE0000) >> 16;
 			intrtx = intr_ep & 0xFFFF;
-#elif defined(CONFIG_USB_AM335X)
+#elif defined(CONFIG_USB_MUSB_AM335X)
 			/* Read TI registers for AM35X, not MUSB registers */
 			intr_ep = regs->irq_status0;
 			regs->irq_status0 = intr_ep;

include/configs/am335x_evm.h
@@ -134,7 +134,7 @@ to a higher value if a higher sized RAM support is available in Hardware */
 #define CONFIG_USBD_PRODUCT_NAME        "EVM"
 #endif /* CONFIG_MUSB_UDC */
 
-#endif /* CONFIG_USB_AM335X */
+#endif /* CONFIG_USB_MUSB_AM335X */
 
 /* commands to include */
 